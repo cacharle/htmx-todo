66751b215b102c8af264c0861bc744a0ba62a5a3
Charles Cabergs <me@cacharle.xyz>
2023-10-07 10:58:24 +0200

    Added checkbox and cross done todos

--- main.go
+++ main.go
@@ -7,27 +7,37 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/template/html/v2"
 )
 
-var todos []string = make([]string, 0)
+type TodoItem struct {
+	Content string
+	Done bool
+}
+
+var todos []TodoItem = make([]TodoItem, 0)
 
 func homeHandler(c *fiber.Ctx) error {
-	return c.Status(200).Render("views/index.html", fiber.Map{"Todos": todos})
+	return c.Status(200).Render("index", nil)
 }
 
 var todoElementsTemplate = template.New("todo-elements")
 
 func addTodoHandler(c *fiber.Ctx) error {
-	todo := strings.Clone(strings.TrimSpace(c.FormValue("todo")))
-	if todo != "" {
-		todos = append(todos, todo)
+	todo_content := strings.Clone(strings.TrimSpace(c.FormValue("todo")))
+	if todo_content != "" {
+		todos = append(todos, TodoItem{Content: todo_content, Done: false})
 	}
-	return c.Status(200).Render("views/todo_elements.html", todos)
+	return c.Status(200).Render("todo_elements", fiber.Map{"Todos": todos})
 }
 
 func main() {
+	todos = append(todos, TodoItem{Content: "Some task", Done: false})
+	todos = append(todos, TodoItem{Content: "Some other task", Done: true})
+	viewEngine := html.New("./views", ".html")
 	template.Must(template.ParseGlob("views/*"))
-	app := fiber.New()
+	// log.Fatal(tpl.ExecuteTemplate(os.Stdout, "todo_elements.html", todos))
+	app := fiber.New(fiber.Config{Views: viewEngine})
 	app.Static("/static", "./static")
 	app.Use(logger.New())
 	app.Get("/", homeHandler)
